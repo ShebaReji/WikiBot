d0ef828fac99c341818d10195d92950836df8ace
Sheba Reji <150365005+ShebaReji@users.noreply.github.com>
2025-06-22 01:07:07 +0530

    Update server.py

--- WikiBot/backend/server.py
+++ WikiBot/backend/server.py
@@ -9,6 +9,7 @@ from langchain.chains import RetrievalQA
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os
 import asyncio
+from dotenv import load_dotenv 
 from fastapi.responses import StreamingResponse
 import json
 
@@ -20,7 +21,7 @@ set_debug(True)
 
 # Set your Google API key here
 load_dotenv()
-os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+os.environ["GOOGLE_API_KEY"] = "GOOGLE_API_KEY" #Put your API KEY HERE
 
 app = FastAPI()
 
